c4445de65d448da5fc917ac8ffcf338b95e620cb
chromefinch <38330372+chromefinch@users.noreply.github.com>
2023-10-16 11:14:53 -0400

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,7 +2,7 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
+    name: 'Chromefinch',
     description: 'The official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
     listUrl: 'https://chromefinch.github.io/kasm-registry/',
