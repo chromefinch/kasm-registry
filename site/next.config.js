b81e2691031da249aff63b9c23f2aef66b6e9c1a
chromefinch <38330372+chromefinch@users.noreply.github.com>
2023-10-16 10:58:52 -0400

    Update next.config.js
    i suck at this

--- site/next.config.js
+++ site/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     name: 'Kasm Technologies',
     description: 'The official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://chromefinch.github.io/kasm-registry/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
